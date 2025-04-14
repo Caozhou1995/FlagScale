ee3b7dd990fc2b396b23aecfb0033c467a0a7e89
caozhou <caozhou@baai.ac.cn>
2025-04-14 16:34:26 +0800

    switch submodule commit

--- __init__.py
+++ __init__.py
@@ -8,7 +8,6 @@ from flagscale.runtime import create_symlinks, init_submodule
 src = os.path.join(os.path.dirname(__file__), "flagscale", "train", "Megatron-LM")
 dst = os.path.join(os.path.dirname(__file__), "third_party", "Megatron-LM")
 init_submodule(dst, "Megatron-LM")
-raise ValueError()
 create_symlinks(src, dst)
 
 
